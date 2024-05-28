85d2d782d6e2cbf1e504916206367bf99f2d32be
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-05-28 20:01:44 +0000

    finished

--- src/js/component/ListItem.jsx
+++ src/js/component/ListItem.jsx
@@ -4,11 +4,11 @@ import React from "react";
 
 const ListItem = (props) => {
 	return (
-		props.list.map((item, index) => {
+		props.list.map((element) => {
 			return (
-				<li key={item + index} className="d-flex justify-content-between">
-					<span className="list-group-item w-100 border-end-0 border-start py-3">{item}</span>
-					<button onClick={() => props.remove(item)} className="btn border border border-start-0 rounded-0">X</button>
+				<li key={element.id} className="d-flex justify-content-between">
+					<span className="list-group-item w-100 border-end-0 border-start py-3">{element.label}</span>
+				    <button onClick={()=> props.delete(element)} className="btn border border border-start-0 rounded-0">X</button>
 				</li>
 			)
 		})
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,49 +1,95 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ListItem from "./ListItem";
 
 
 const Home = () => {
 
-	const [listItem, setListItem] = useState("");
+	const [menssagge, setMenssagge] = useState(false);
 	const [list, setList] = useState([]);
-	const [menssagge, setMenssagge] = useState(true);
+	const [listItem, setListItem] = useState(
+		{
+			"label": "",
+			"is_done": false
+		}
+	);
+
+	function addItemList(event) {
+		setListItem({ ...listItem, [event.target.name]: event.target.value })
+	}
 
 
-	function addItemList(e) {
-		if (e.key == "Enter") {
-			setList([...list, listItem])
-			setMenssagge(false)
+	async function getList() {
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users/nestor-frones");
+			const data = await response.json();
+			setList(data.todos);
+			if (data.todos.length === 0) setMenssagge(true);
+		} catch (e) {
+			console.log(e);
 		}
 	}
 
-	function removeItemList(listItemToRemove) {
-		const newList = list.filter((item) => item !== listItemToRemove);
-		setList(newList);
-		if (newList.length === 0) {
-			setMenssagge(true)
-		}
-		if (newList.length !== 0) {
-			setMenssagge(false)
+	async function handleSubmit(e) {
+		e.preventDefault()
+		const response = await fetch("https://playground.4geeks.com/todo/todos/nestor-frones", {
+			method: "POST",
+			body: JSON.stringify(listItem),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		if (response.ok) {
+			getList();
+			setMenssagge(false);
+		} else {
+			alert("no se puede crear");
 		}
 	}
+
+
+	async function deleteItemList(element) {
+		const response = await fetch('https://playground.4geeks.com/todo/todos/'+`${element.id}`, {
+			method: "DELETE",
+		})
+		if (response.ok) {
+			getList();
+		} else {
+			alert("no se puede eliminar");
+		}
+	}
+
+	
+
+
+	useEffect(() => {
+		getList();
+	}, []);
+
 
 
 	return (
 		<div className="container my-5">
 			<div className="card border-0 shadow py-3 mb-5 bg-body-tertiary">
 				<h1 className="title text-center p-3">todos</h1>
-				<input className="w-100 p-3 border" type="text" placeholder="ingrese tarea" onChange={(event) => setListItem(event.target.value)} onKeyDown={(e) => addItemList(e)} />
+				<form onSubmit={(event) => handleSubmit(event)}>
+					<input className="w-100 p-3 border" type="text" value={listItem.label} name="label" placeholder="ingrese tarea" onChange={(event) => addItemList(event)} />
+				</form>
 				<ul className="list-group list-group-flush w-100 d-flex justify-content-between">
 					<h5 className={menssagge ? "" : "d-none"}>No hay tareas pendientes</h5>
-					<ListItem list={list} remove={removeItemList} />
+					<ListItem list={list} delete={deleteItemList} />
 				</ul>
-				<div id="emailHelp" className={menssagge ? "d-none form-text" : "form-text"}>Quedan {list.length} tareas pendientes!</div>
+				<div id="emailHelp" className={menssagge ? "d-none form-text" : "form-text"}> Quedan {list.length} tareas pendientes!</div>
 			</div>
 		</div>
 	)
 };
 
 export default Home;
+
+
+
+
+
 
 
 
